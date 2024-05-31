0a22fc7b25680debf8202ec5024d585698545a21
Alexey Kudryashov <v-alexeyk@zillowgroup.com>
2024-06-01 00:31:43 +0300

    Fix Logo

--- app/(root)/_components/Logo.tsx
+++ app/(root)/_components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-1">
-      <Image src="/assets/images/site-logo.svg" alt="DevFlow" width={23} height={23} />
+      <Image src="/assets/images/site-logo.svg" alt="WAnswers" width={23} height={23} />
       <p className="h2-bold font-spaceGrotesk max-sm:hidden">
         W<span className="text-orange-500">answers</span>
       </p>
